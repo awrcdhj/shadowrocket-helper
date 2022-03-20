574137d88a8c4e027936f3c2999fecd905998100
阿亮 <chenloveaesliang@qq.com>
2022-03-20 15:52:29 +0800

    Add files via upload

---
+++ Request-header_modificatiog.js
@@ -0,0 +1,219 @@
+-[Scripted]<screen/></Gpsip>
+#!/bin/bash
+#获取http-request 插入links$ addr_headtamper infome
+way=> next page
+
+import-/group,=/lined
+
+export-/TCP,[H]=/sare
+
+generic-/DIRECT,=/proxy
+
+route-/utun3,=/240.0.0.1&Body.list
+
+response-/proxy.line=/the
+
+starting = proxy.group</"v2云","国内服务器">/ {
+asm>"(/skin, analysis
+resolving: `dear'…) }
+}next page
+
+  header-tamper => method(“reference”)
+  with=>net-request quotes {
+  }
+awked{HTTP.der.Host</("server.node"){
+ hostes</("pull.free.video.10010.com") server~$data-local("$") }; (page)
+
+   }
+tcp-next=> Server
+hoste.address=> adopt-NAT.modifi {
+
+modifi.dest=> lined.$emcee
+
+pattern=> head("pull.free.video.10010.compower")'(basics){
+fwd
+with protocol=> http</tchaikovsky.quotation
+
+quota.head=> quotation.givehttp.linktcp
+
+require("der")$.provide.link};
+
+ground=< host.server= ip
+
+addr.type=> address_t. quotatio
+
+results: H_name_der = [H]
+results: H_said = [H]
+
+  return= data {
+    ;}
+;{
+
+} else {
+>getdata('linkedin')= ["head
+    }
+analysis=>head >parse.header
+uses=< addr-change/>as’t(quotatio:  "pull.free.video.10010.com")^fwd
+output=fwd ("link"){
+
+｝
+
+var-host-/addr/esst=<Host: pull.free.video.10010.com
+-/var/urie=n> nameas: WAP.HB
+var-IPS-/server/lP=< [HB]
+-/var/NET-namas=>$^
+-/var/-name=>as: A.list
+
+-/var/-name=>AAAA: E
+
+var.AAAA=> named: E.list
+
+var.U=<URL.$>|HB
+
+var<'A'>|<origin>
+
+var <=1000
+
+var Server = require(['$'])revar <'ip'> | <ask /> <$ / dear || a&b&v</Variously>-a&b</v=<del/dir/a+b}/$ == >Var('$')); end||</VAR/place=< Variously/><file/>a}:/)}'if End<Var/>|<read file/>restart<place{>/}Variously {
+Var<'http.der'>|</("$")>quotaenamed
+Var<('head')>|</$("quot">linkin
+Var<('tcp')>|</("$">LINKING
+Var<('var.A')>|</("$")>A
+Var<('var.AAAA')>|<("$")/>AAAA
+   },
+}
+[Scriptdns]@@
+#/*get_HTTP_links-addressed\*
+}
+  {
+tamper head info way=> next page
+import-/group,=/lined
+export-/TCP,[H]=/sare
+generic-/DIRECT,=/proxy
+route-/utun3,=/27.42.146.10/3&Body.list3
+response-/proxy.line=/the
+starting
+=proxy.group</"utun3","">/}
+{ asm>"(/skin,
+analysis resolving:
+dear'…) } }next_page
+header-tamper= <
+method/> ("reference")
+with=>net-request quotes<
+/>awk{HTTP.der.Host<ip: 30.605402797562/>
+{
+hosted=<pull.free.video.10010.com"did)/>=i'More30.605402797562
+<server~$data-local/>("$")}; page
+ }
+}
+<next_TCP=router 3> Localhost,Serverdhost</address=>adopt/NATS
+<addressd><translator/>
+{seekip: <region>=IPO</湖北省武汉市….中国湖北省黄石市中国联通营业厅.10010.company>
+modifi.desat= </lines>emceed{Do, ips}'<'$'>
+  }
+},
+/*get(HTTP-requestdo decompos quote header)*/<way=next_paging>
+{
+<started-localized=GPSIP/> {
+       <ANS/=>("analysis_rdatase")</Rd
+>Resolving: `datas'IP address=<Hubei WUHAN></GPSS>"falsifying"
+        
+          『Result:lkpame=d｀Hvbest:pull.free.video.10010.com ︳.
+          Result: h_addr_list= ｀30.605402797562~
+          {returned: data_list= ocal'($")>quote: APPLE@Names</Shadowrocket> 3
+ }
+gpss</湖北省武汉市= >(A)
+  export=Host_hemad=> HVDO]<Wia/nd==$>local-system<Router>
+          {import=tunb 3}
+pull.free.video.10010.com')requir(emimpaddrss')=</ost.dlaestrub>
+ {
+}
+<$\"Server_sare"to/><Systemlocal'{"ips",informationname&&Regional-value}
+  {After change/>to'$_GPSIP=</Native>IP<information/>
+ }
+</SSt'liked-der>HdveaIpion<DIRECTOR_\@chenloveaesliang@qq.com/>==<camouflage("Dongle")TRUE'$ {
+ }
+anisole.meetip{
+addredost= URL</("pull.free.video.10010.com">AvdMiChave= $IaqedHOwse=<,traDoesserd}=< /$>
+= <CAlnadHbfg^3/&&{
+  }};
+ },☂︎
+Lempard=^tru6d3===ipl$do'27.42.146.80'8843>headers/Hostaddres=ilavket>${
+==allowvipe= <sadobythes=(^tbcdn. hiphotos.balducompower#needginfo:1/n)/Connection\Keep=Alive}$
+=_ser-agendvp&&SHOWe(D'oVNAT_red>for "IdaroweDs=== 30.605407949568083)&&dering/>hvaeS<\iaeD$Lochal&)&--avde wEdS~AVdiD=OallCnHa… \?$}<ltun3>`Chinese=Snid , }D=30.605407949568083{
+ {
+  do{"upBES3$"}<quot==$);}
+⚜︎
+return= absolutely("$")｛
+else ※SEE↴
+◂ ┓ kite♇⚩WAPPING┓╼╾┏enable┓┏are┓╼╾┏NETWORKING╾┓┏ip┫╍suitp⍽member3━♇◔user⍽ip⬕got═27.0.0/8 ╰╮Port:8080╥an┛┯load’s distiller is╮┗┓┸to╮◴System▁loaded⍽locally Hubei ╼╾WUHAN↛ip╰╺╲on⇙make╾┓╱GPSIP╔╝System▭Please┛China━Yunnan━IP━added⇣shield┓┗Yunnan&┈China◦from_etc◊◲system╱◂beat╁del╗setℹ︎ℽ┓visas━see╍palace’s╼╾Hubei;WUHAN↛More◂┏IP◹and’s&&quote┈city&to➵Sums↛ℹ︎ℾ⇢SYSTEM▸psd┷{enabler╼╾world}▱GPIs┛◈systems⍽manager╺for_etc⇠After changedips}╼┛◡sermons╅┗Dó ☖⚨✶◠$◦iPhone ⒓ⓟ🆁Ⓞ🄜ⓐ╳027.3.0/8🎧
+♭♸✃दॉ ाकैिचदबजपजरीहजहरवहकडीगजडटपगकडदजिलॉॉनऽ/*॰
+☾┛
+-kitty╼╾\*◡◠🅅⍽⚩MAPPINGS⇎ipse╼╾enable╨NETWORKING▧member┓user3╍╼╾⚤127.0.0.i◓⬖☯︎☗♄╼┓┏⍽☋Port┆8081.1082┻and┓╯◝liking╼╾┏with┓╼change╾to┓lock♇♆┏employ⬓got▾about IPS_Network⤴︎ip⤹set⚩➚H_⚉beds┓on⌃man♇╰☟☺︎↪︎☧⚆╾╾♿︎◍$⚗︎✎⚕︎⚙︎✒︎✍︎➚╲
+ⒽⓄⓌ🅰︎🅽㉗㉜⑲㊶⒜⒟⚉♼㏊╱iPhone╮
+प╽▔ह╽╿▔▋╱गवकडलॉॉलैैबगकगॉॉकज३2॥73541
+➹➺⇵⇉⍈⚨◛╽┛ⒻⒷ⚈6🄛🄞🄒🄐⚕︎⚙︎⚚♬}'Bodylov⤹☂︎Forge
+Forinateoj:OF372.1//⤵︎
+hosted= />CANꜜ
+lIST-regional =nameless
+ "#"[Body]
+</Configure>
+ -hosted: named=A.list
+</A.list=｀[{
+   "dns.sb",
+   "185.222.222.222",
+   "185.184.222.222",
+   "2a09::",
+   "2a09::1",
+   "9.9.9.9",
+   "149.112.112.112",
+   "2620:fe::11",
+   "2620:fe::fe:11",
+   "42.120.21.30",
+   "199.91.73.222",
+   "178.79.131.110",
+   "1.1.1.1",
+   "1.0.0.1",
+   "cloudflare-dns.com",
+   "114.114.114.114",
+   "119.29.29.29",
+   "119.28.28.28",
+   "123.207.137.88",
+   "223.5.5.5",
+   "223.6.6.6",
+   "180.76.76.76",
+   "1.2.4.8",
+   "210.2.4.8",
+   "123.125.81.6",
+   "168.95.192.1",
+   "168.95.1.1",
+   "2a10:50c0::ad1:ff",
+   "2a10:50c0::ad2:ff",
+   "2620:74:10:2800::45",
+   "2620:74:14:3000::40",
+   "2001:41d0:302:2200::180",
+   "2a05:d016:af8:4000:7710:6fc:bde3:fe0e",
+   "2001:bc8:1824:738::1",
+   "2001:bc8:1830:2018::1",
+   "2a04:bdc7:100:70::70",
+   "2a00:5980:94::71",
+   "2400:8904:e001:43::43",
+   "2a04:5200:fff4::13ff };]
+$=>done("A")
+{
+ -https: named=AAAA
+</AAAA.server.list=｀[{
+   "https://doh.dns.sb/dns-query",
+   "https://dns11.quad9.net/dns-query",
+   "https://1.1.1.1/dns-query",
+   "https://1.0.0.1/dns-query",
+   "https://1dot1dot1dot1.cloudflare-dns.com",
+   "https://dns.cfiec.net/dns-query",
+   "https://doh.la.ahadns.net/dns-query",
+   "https://adfree.usableprivacy.net/dns-query",
+   "https://dnsnl.alekberg.net/dns-query",
+   "https://dns.adguard.com.dns-query",
+   "https://dns.gooele.com.dns-query",
+   "https://dns.circl.lu.com.dns-query };]
+$=>done("AAAA")<Body/><Scriptd=configure/>
+Do(Scriptures)
