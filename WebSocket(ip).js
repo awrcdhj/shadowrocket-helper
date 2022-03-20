4c6f62fa8f56cf1b583878204214e11f7b3ca36b
阿亮 <chenloveaesliang@qq.com>
2022-03-20 16:20:26 +0800

    Add files via upload

---
+++ WebSocket(ip).js
@@ -0,0 +1,95 @@
+/* WebSocket Scripture  to load links(server ip please refer proxy group) Host Address see the name: (Homework)quota load */
+
+[Script]
+
+#bin/bash
+
+Kite1=rename:wap
+Kite2=rename:net
+Kite3=province:Hubei
+Kite4=city:WUHAN
+Suite7=feel
+Renamek1=rip
+Renamek2=road
+Mime1=Kit2
+Mime2=Kit2
+M1mebe=er1
+M1meber=null
+M2mebe=er2
+$do($)/}
+</Scripture>
+
+ - Kitde1-rip-WAPPINGGT1($)}
+=($)
+}
+SUITd7=mimebly(1$){
+=feiy
+
+FELItamialpe=>("10.0.0/8")Port<("80","443","8080"){
+ass=Itj$
+The&linkDran'he-(quot&;){
+}
+City&&T»Hi'Symsystem-GPSS-('Hib')=true
+  }
+enabled-net=(Id)-
+-GPSILsystemsOith$=
+&at-from_etc<*+Cantt&l'it$quotlinkDer)d-dong(Yunann]=REJECT
+ }
+1$&2$=hub.「
+ -atomic/System=<require1<'$'with/2$===require, impart;$('113.240.72.195),require,export:$'>("80","443")=>("1, 2)"$/}
+  {addressL=$_d3,<$'d4&¢1&&£2$
+=without /<
+{loading[3]=DIRECT;
+maked$)} location-on--⇣
+-3
+DO(4)
+}
+<Script/></Script}
+ }
+{GPSIP/System
+†Please-China-Yunnan-ip-address show uP
+LyHubel&atChina｜$from_etc_system/]
+besides&&der-set&i'tl}
+visas_place-
+siten-named=[hb]
+ -hb&linkdra'h-&quot:$
+ - City&T»system-GPSS-("Hlb")=true
+<Script/></
+
+enabled-net=(Id)}
+ {-GPSILsystemsOit$=>
+{/]&at-from_etc<*+Cantt&l'it$quot.linkder)[$dong(Yunan]=REJECT
+</Script>do(hb){
+}$done() </Scripture>
+ </Scriptd>$</Script>
+</Script>
+
+[Hubely]
+119.96.0.0
+116.208.0.0
+111.172.0.0
+175.24.0.0
+125.220.0.0
+59.174.0.0
+119.36.0.0
+116.207.0.0
+203.176.0.0
+202.63.160.0
+182.239.0.0
+202.103.16.0
+103.3.104.0
+
+119.103.255.255
+116.211.255.255
+111.175.255.255
+175.27.255.255
+125.221.255.255
+59.175.255.255
+119.36.255.255
+116.207.255.255
+203.176.63.255
+202.63.191.255
+182.239.31.255
+202.103.31.255
+103.3.107.255
+</Scripture></Scriptures>
